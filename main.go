c07ddb18d9f00dc378982187346be8bad56f846c
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2025-01-30 16:29:00 +0300

    feat:added a health checker

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World! welcome to cloud run, yeah it works well")
 }
 
+// healthCheckHandler responds to health check requests
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "healthy")
+}
+
 func main() {
 	port := 8080
 	fmt.Println("Server is running on port", port)
 
 	http.HandleFunc("/test", handler)
+	http.HandleFunc("/health", healthCheckHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server error:", err)
 	}
